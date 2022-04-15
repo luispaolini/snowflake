d2d58f593218d779745f35a06f3888a4cfd1800b
luispaolini <luis.paolini@gmail.com>
2022-04-15 14:58:03 -0300

    Convert Snowflake data from JSON

---
+++ src/main/java/com/example/snowflake/model/Customer.java
@@ -0,0 +1,22 @@
+package com.example.snowflake.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Customer {
+    @JsonAlias({ "C_CUSTKEY" })
+    private Long cCustkey;
+
+    @JsonAlias({ "C_NAME" })
+    private String cName;
+
+    @JsonAlias({ "C_ADDRESS" })
+    private String cAddress;
+}
